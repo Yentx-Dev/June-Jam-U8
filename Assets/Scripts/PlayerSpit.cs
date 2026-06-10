09bed8e9d02af465503bd9998955459ef80ccb4e
Yentx-Dev <dylanng396@gmail.com>
2026-06-10 22:13:50 +0800

    V 1.8
    - Modified PlayerSpit, so that it can eat trash and gain size too

--- Assets/Scripts/PlayerSpit.cs
+++ Assets/Scripts/PlayerSpit.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Drawing;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,27 +6,30 @@ using UnityEngine.InputSystem;
 public class PlayerSpit : MonoBehaviour
 {
     public int spitStrength;
-    public float minSize;
-    public int maxSpits;
-    private float maxSize; // set to what the player is originally on start
+    public float minSize =0.5f;
+    public int maxSpits = 5;
+    public float maxSize =1f; // set to what the player is originally on start
     private float sizeDelta;
     private InputAction spitAction;
     public GameObject trashObject;
     private ThrowableTrash currTrash;
     public Transform spawnPoint;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         spitAction = InputSystem.actions.FindAction("Spit");
-        maxSize = GetComponent<Transform>().localScale.x;
-        sizeDelta = (maxSize - minSize)/(maxSpits);
-        spitAction.started += context =>{
+        //maxSize = GetComponent<Transform>().localScale.x;
+        sizeDelta = (maxSize - minSize) / (maxSpits);
+        spitAction.started += context =>
+        {
             Transform playerTransform = GetComponent<Transform>();
             float currSize = playerTransform.localScale.x;
-            if (currSize <= minSize) {return;} // Don't spit if at min size
+            if (currSize <= minSize) { return; } // Don't spit if at min size
             spitTrash();
-            float newSize = (float) Math.Max(minSize, currSize - sizeDelta);
+            float newSize = Mathf.Max(minSize, currSize - sizeDelta);
             playerTransform.localScale = new Vector3(newSize, newSize, newSize);
+            //sphereCollider.radius = newSize / maxSpits * sCollider ;
             Debug.Log("Trash was spit");
         };
     }
@@ -35,9 +37,9 @@ public class PlayerSpit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
     private void spitTrash()
     {
         // Spawn trash
@@ -46,5 +48,27 @@ public class PlayerSpit : MonoBehaviour
         // Throw trash
         currTrash.Throw(spawnPoint.up, spitStrength);
         Destroy(spawnedTrash, 5);
+    }
+
+    private void eatTrash(GameObject trash)
+    {
+        Transform playerTransform = GetComponent<Transform>(); //access transform component
+        float currSize = playerTransform.localScale.x; //store current size of trashbag
+
+        if (currSize >= maxSize) { return; } //stops code if current size is equals to maxSize
+
+        float newSize = Mathf.Min(maxSize, currSize + sizeDelta);
+        playerTransform.localScale = new Vector3(newSize, newSize, newSize);
+
+        Destroy(trash);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("trash"))
+        {
+            Debug.Log("Yum");
+            eatTrash(other.gameObject);
+        }
     }
 }
